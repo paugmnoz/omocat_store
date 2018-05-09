73f3aac8e69e11027068be1635c9a4c844cee48f
Paula <paulaagarcia1@gmail.com>
2018-05-09 07:30:59 -0500

    Checkout

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -4,7 +4,7 @@ arreglo.forEach(function(elemento){
     lista.innerHTML += '<li>'+ elemento + '</li>';
 });*/
 
-fetch('http://localhost:1889/productosPorIds?ids' + arreglo)
+fetch('http://localhost:1889/productosPorIds?ids=' + arreglo)
     .then(function (res) {
         return res.json();
     })
@@ -13,7 +13,6 @@ fetch('http://localhost:1889/productosPorIds?ids' + arreglo)
 
         var lista = document.querySelector('.lista');
         res.forEach(function (elem) {
-            lista.innerHTML += '<li><img width="100" src="'
-             + elem.img_principal + '">' + elem.nombre + '</li>';
+            lista.innerHTML += '<li>' + elem.nombre + '</li>';
         });
     });
