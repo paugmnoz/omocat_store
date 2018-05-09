ab360742facab110ab060073349830591c69248d
Paula <paulaagarcia1@gmail.com>
2018-05-09 15:03:12 -0500

    lista checkout

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -12,7 +12,25 @@ fetch('http://localhost:1889/productosPorIds?ids=' + arreglo)
         console.log(res);
 
         var lista = document.querySelector('.lista');
-        res.forEach(function (elem) {
+        res.forEach(function (elem, position) {
             lista.innerHTML += '<li>' + elem.nombre + '<button class="delete">X</button>' + '</li>';
+
+            document.querySelectorAll('.delete').forEach(function (button) {
+                button.addEventListener('click', function () {
+                    console.log('holi borrar');
+            
+                    var id = button.parentElement.getAttribute('data-id')
+               
+                    //agregar al arreglo
+                    arreglo.splice(position, 1);
+                    console.log('se fue al carrito');
+                    actualizarCarrito();
+                    localStorage.setItem('arreglo', JSON.stringify(arreglo));
+                    lista.innerHTML = "";
+                 
+                });
+
+            });
+
         });
     });
